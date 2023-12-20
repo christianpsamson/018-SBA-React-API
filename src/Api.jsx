5d584a271f27f299dba4ff1590a13cfa40c60503
Christian Samson <christiansamson@Christians-MBP.ptd.net>
2023-12-20 18:29:08 -0500

    Updated the .env

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,8 +1,8 @@
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const GEO_API_URL = import.meta.env.VITE_REACT_APP_GEO_API_URL;
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "b94a23ccb4msh2d5b9f5673899b2p1edb79jsnc72dbb569f22",
+    "X-RapidAPI-Key": import.meta.env.VITE_REACT_APP_GEO_API_KEY,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
